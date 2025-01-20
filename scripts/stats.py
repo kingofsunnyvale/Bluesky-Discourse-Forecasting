d3ab9b44f2bfcc93cc6d3b89043b608e4dfd56f9
Avnees Muralitharan <work@169-231-218-41.wireless.ucsb.edu>
2025-01-20 09:35:42 -0800

    made hist look better

--- scripts/stats.py
+++ scripts/stats.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-parquet_filename = "../data/2025-01-20.parquet"
+df = pd.read_parquet("../data/2025-01-20.parquet")
 
-# load the parquet into pandas
-df = pd.read_parquet(parquet_filename)
-
-# create a histogram based on the timestamp 
+# Make histogram of the number of posts per hour of the day
 df['timestamp'] = pd.to_datetime(df['createdAt'], format='mixed', utc=True)
+
 df['hour'] = df['timestamp'].dt.hour
-df['hour'].hist(bins=24)
-plt.xlabel('Hour of the day')
+
+hour_counts = df.groupby('hour').size().reindex(range(24), fill_value=0)
+
+plt.figure(figsize=(10, 6))
+plt.bar(hour_counts.index, hour_counts.values, 
+        width=0.8, edgecolor='black', color='cornflowerblue')
+
+plt.xlabel('Hour of the day (UTC)')
 plt.ylabel('Number of posts')
 plt.title('Number of posts per hour of the day')
-plt.show()
+plt.xticks(range(24))
 
+plt.grid(axis='y', alpha=0.5)
+plt.show()
