87310a642a6abf6c0c0d83fec073220bfd118178
Avnees Muralitharan <work@Avneeshs-MacBook-Pro.local>
2025-01-22 12:46:51 -0800

    Only plotting post counts from 01-20

--- scripts/stats.py
+++ scripts/stats.py
@@ -4,10 +4,12 @@ from matplotlib.ticker import FuncFormatter
 
 df = pd.read_parquet("../data/2025-01-20.parquet")
 
-# Make histogram of the number of posts per hour of the day
+# Make histogram of the number of posts per hour of the day, only considering posts from 2025-01-20
 df['timestamp'] = pd.to_datetime(df['createdAt'], format='mixed', utc=True)
 df['hour'] = df['timestamp'].dt.hour
-hour_counts = df.groupby('hour').size().reindex(range(24), fill_value=0)
+df['date'] = df['timestamp'].dt.date
+df_filtered = df[df['timestamp'].dt.date == pd.to_datetime('2025-01-20').date()]
+hour_counts = df_filtered.groupby('hour').size().reindex(range(24), fill_value=0)
 
 plt.figure(figsize=(10, 6))
 plt.bar(hour_counts.index, hour_counts.values,
